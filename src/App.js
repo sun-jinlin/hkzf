ed4e88c2071c9867adc5e16fe3339017056dbb9e
jinzi <468762932@qq.com>
2021-02-06 19:28:45 +0800

    基本一级路由配置

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+// 引入路由
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+// 引入一级路由组件
+import Home from './page/Home'
+import CityList from './page/CityList'
+import Map from './page/Map'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      {/* 路由导航 */}
+      <Link to="/home">首页</Link>
+      <Link to="/citylist">城市列表</Link>
+      <Link to="/map">地图</Link>
+
+
+      {/* 路由配置 */}
+      <Route path="/home" component={Home}></Route>
+      <Route path="/citylist" component={CityList}></Route>
+      <Route path="/map" component={Map}></Route>
+    </Router>
   );
 }
 
